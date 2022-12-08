7176293bdfd1265a7b62ad0f5ef08f2c0183998c
AndOliver46 <anderson_souza75@live.com>
2022-12-08 13:18:00 -0300

    alterando imagens com click na pagina bicicleta

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 //Ativar links menu
-const links = document.querySelectorAll('.header-menu a');
+const links = document.querySelectorAll(".header-menu a");
 
-function ativarLink(link){
-  if(location.href.includes(link.href)){
+function ativarLink(link) {
+  if (location.href.includes(link.href)) {
     link.classList.add("header-active");
   }
 }
@@ -12,10 +12,10 @@ links.forEach(ativarLink);
 //Ativar itens orçamento
 const parametros = new URLSearchParams(location.search);
 
-function ativarProduto(parametro){
+function ativarProduto(parametro) {
   //const elemento = document.querySelector("#" + parametro);
   const elemento = document.getElementById(parametro);
-  if(elemento){
+  if (elemento) {
     elemento.checked = true;
   }
 }
@@ -25,21 +25,42 @@ parametros.forEach(ativarProduto);
 //Perguntas frequentes
 const perguntas = document.querySelectorAll(".perguntas button");
 
-function ativarPergunta(event){
+function ativarPergunta(event) {
   const pergunta = event.currentTarget;
   const controls = pergunta.getAttribute("aria-control");
   const resposta = document.getElementById(controls);
   resposta.classList.toggle("ativa");
 
-  if(resposta.classList.contains('ativa')){
-    pergunta.setAttribute('aria-expanded', 'true');
-  }else{
-    pergunta.setAttribute('aria-expanded', 'false');
+  if (resposta.classList.contains("ativa")) {
+    pergunta.setAttribute("aria-expanded", "true");
+  } else {
+    pergunta.setAttribute("aria-expanded", "false");
   }
 }
 
-function eventosPerguntas(pergunta){
+function eventosPerguntas(pergunta) {
   pergunta.addEventListener("click", ativarPergunta);
 }
 
 perguntas.forEach(eventosPerguntas);
+
+//Imagens bicicletas
+const imagens = document.querySelectorAll(".bicicleta-imagens img");
+const imagensContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  if (
+    matchMedia("(min-width:920px)").matches ||
+    (matchMedia("(min-width:460px)").matches &&
+      matchMedia("(max-width:800px)").matches)
+  ) {
+    imagensContainer.prepend(img);
+  }
+}
+
+function imagemClick(imagem) {
+  imagem.addEventListener("click", trocarImagem);
+}
+
+imagens.forEach(imagemClick);
